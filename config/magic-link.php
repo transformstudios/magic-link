c05af68fccdb10955c5d868df6d12243a7739475
edalzell <erin@thedalzells.org>
2021-06-04 10:49:33 -0700

    default the redirect to app url

--- config/magic-link.php
+++ config/magic-link.php
@@ -2,5 +2,5 @@
 
 return [
     'email_subject' => env('MAGIC_LINK_EMAIL_SUBJECT', 'Magic Login Link'),
-    'redirect' => env('MAGIC_LINK_REDIRECT'),
+    'redirect' => env('MAGIC_LINK_REDIRECT', env('APP_URL')),
 ];
